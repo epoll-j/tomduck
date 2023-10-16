4521975134dff81b040ca97b32e3da416803dfd3
Dubhe <epoll@foxmail.com>
2023-10-16 10:25:50 +0800

    Update ProxyService.swift

--- ios/Runner/ProxyService/ProxyService.swift
+++ ios/Runner/ProxyService/ProxyService.swift
@@ -50,8 +50,8 @@ public class ProxyService: NSObject {
             .childChannelOption(ChannelOptions.connectTimeout, value: TimeAmount.seconds(10))
     }
     
-    public static func create() -> ProxyService? {
-        return ProxyService(task: Task())
+    public static func create(_ id: NSNumber) -> ProxyService? {
+        return ProxyService(task: Task(id: id))
     }
     
     public func run(_ callback: @escaping ((Result<Int, Error>) -> Void)) -> Void {
